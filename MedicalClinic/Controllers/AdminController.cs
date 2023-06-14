8323f6bd05b694f2767fba4a806a0d1485e51b5a
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-14 23:18:21 +0500

    some changes on admin panel

---
+++ MedicalClinic/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalClinic.Controllers;
+public class AdminController : Controller
+{
+    public IActionResult Admin()
+    {
+        return View();
+    }
+
+    public IActionResult AddDoctor()
+    {
+        return View();
+    }
+
+    public IActionResult Doctors()
+    {
+        return View();
+    }
+
+    public IActionResult ClinicInformationSettings()
+    {
+        return View();
+    }
+
+
+}
